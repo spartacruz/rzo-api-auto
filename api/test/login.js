70d33ca34ce41a9faaebe1ecd05d194cd3d6a305
Yuri <yuri.barru@gmail.com>
2023-07-20 21:24:42 +0700

    [login] - login, add testcase

--- api/test/login.js
+++ api/test/login.js
@@ -17,6 +17,64 @@ const testCase = {
 };
 
 describe(`Login Test`, () => {
+    it(`${testCase.positive.LO_1}`, async function() {
+        const response = await page.postLogin({
+            email: "eve.holt@reqres.in",
+            password: "cityslicka"
+        });
+        expect(response.status).equal(200);
+        checkStructure(response.body);
+    });
+
+    it(`${testCase.negative.LO_2}`, async function() {
+        const response = await page.postLogin({
+            email: "eve.holt@reqres.in",
+            password: "asd"
+        });
+        expect(response.status).equal(400);
+        expect(response.body).to.deep.equal({});
+    });
+
+    it(`${testCase.negative.LO_3}`, async function() {
+        const response = await page.postLogin({
+            email: "yuri@reqres.in",
+            password: "cityslicka"
+        });
+        expect(response.status).equal(400);
+        expect(response.body.error).to.equal("user not found");
+    });
+
+    it(`${testCase.negative.LO_4}`, async function() {
+        const response = await page.postLogin({
+            email: "yuri@reqres.in",
+            password: "asd"
+        });
+        expect(response.status).equal(400);
+        expect(response.body.error).to.equal("user not found");
+    });
+
+    it(`${testCase.negative.LO_5}`, async function() {
+        const response = await page.postLogin({
+            password: "asd"
+        });
+        expect(response.status).equal(400);
+        expect(response.body.error).to.equal("Missing email or username");
+    });
+
+    it(`${testCase.negative.LO_6}`, async function() {
+        const response = await page.postLogin({
+            email: "eve.holt@reqres.in"
+        });
+        expect(response.status).equal(400);
+        expect(response.body.error).to.equal("Missing password");
+    });
+
+    it(`${testCase.negative.LO_7}`, async function() {
+        const response = await page.postLogin({});
+        expect(response.status).equal(400);
+        expect(response.body.error).to.equal("Missing email or username");
+    });
+
     function checkStructure(data_payload) {
         const message = "property does not exist in data"
         expect(data_payload.hasOwnProperty('token')).to.equal(true, `token ${message}`);
