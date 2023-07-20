34543dcdc5b4f05fee0ba7be79c36f72a607e479
Yuri <yuri.barru@gmail.com>
2023-07-20 22:01:29 +0700

    [user] - user-detail, add testcase

--- api/test/user-detail.js
+++ api/test/user-detail.js
@@ -16,6 +16,45 @@ const testCase = {
 };
 
 describe(`User Detail Test`, () => {
+    const DEFAULT_USER_ID = 1
+    it(`${testCase.positive.UG_1}`, async function(){
+        const response = await page.getUserDetail(DEFAULT_USER_ID);
+        expect(response.status).to.equal(200);
+        checkStructure(response.body.data);
+    });
+
+    it(`${testCase.negative.UG_2}`, async function(){
+        const response = await page.getUserDetail(1230);
+        expect(response.status).to.equal(404);
+        expect(response.body).to.deep.equal({});
+    });
+
+    it(`${testCase.negative.UG_3}`, async function(){
+        const response = await page.getUserDetail("a");
+        expect(response.status).to.equal(404);
+        expect(response.body).to.deep.equal({});
+    });
+
+    //not counted
+    it.skip(`${testCase.negative.UG_4}`, async function(){
+        const response = await page.getUserDetail("");
+        expect(response.status).to.equal(404);
+        expect(response.body).to.deep.equal({});
+    });
+
+    it(`${testCase.negative.UG_5}`, async function(){
+        const response = await page.getUserDetail(-1);
+        expect(response.status).to.equal(404);
+        expect(response.body).to.deep.equal({});
+    });
+
+    it(`${testCase.negative.UG_6}`, async function(){
+        const response = await page.getUserDetail(0);
+        expect(response.status).to.equal(404);
+        expect(response.body).to.deep.equal({});
+    });
+
+
     function checkStructure(data_payload) {
         const message = "property does not exist in data"
         const payloadKeys = ['id', 'email', 'first_name', 'last_name', 'avatar']
